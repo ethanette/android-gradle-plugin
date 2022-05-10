f69945e52439cb640aec11f7976e54de89bdcddc
SangJunHan <hsjun85@gmail.com>
2022-05-10 14:38:45 +0900

    chore: modifiy artifact id

--- buildSrc/src/main/java/Configs.kt
+++ buildSrc/src/main/java/Configs.kt
@@ -1,10 +1,10 @@
 object Configs {
 
     const val GROUP_ID = "com.github.ethanette"
-    const val VERSION = "1.0.0"
+    const val VERSION = "0.1.1"
 
     object Catalog {
-        const val ARTIFACT_ID = "android-version-catalog"
+        const val ARTIFACT_ID = "version-catalog"
     }
 
 }
